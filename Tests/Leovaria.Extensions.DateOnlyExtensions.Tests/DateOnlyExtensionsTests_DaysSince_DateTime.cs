4a98937f70f4eb7f34eb4c0f4c89f22bcf663f97
bradley.leopold <bradley.leopold@gmail.com>
2024-03-14 19:46:54 -0400

    Refactored some of the date only extension methods. Added more unit tests for some of the methods.

--- Tests/Leovaria.Extensions.DateOnlyExtensions.Tests/DateOnlyExtensionsTests_DaysSince_DateTime.cs
+++ Tests/Leovaria.Extensions.DateOnlyExtensions.Tests/DateOnlyExtensionsTests_DaysSince_DateTime.cs
@@ -17,6 +17,11 @@
                 { new DateOnly(2024, 01, 02), new DateTime(2024, 01, 01, 05, 01, 35), 1 },
                 { new DateOnly(2024, 02, 11), new DateTime(2024, 02, 01), 10 },
                 { new DateOnly(2025, 01, 01), new DateTime(2024, 12, 31), 1 },
+                { new DateOnly(2025, 01, 01), new DateTime(2023, 12, 31), 367 },
+                { new DateOnly(2025, 01, 01), new DateTime(2025, 01, 02), -1 },
+                { new DateOnly(2025, 01, 01), new DateTime(2025, 01, 01), 0 },
+                { DateOnly.MinValue, DateTime.MaxValue, -3_652_058 },
+                { DateOnly.MaxValue, DateTime.MinValue, 3_652_058 }
             };
         }
     }
